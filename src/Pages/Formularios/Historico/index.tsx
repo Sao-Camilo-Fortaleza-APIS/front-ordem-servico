9ebd76934944aa86f0470f64f8d00de560252448
Myguel Angello <myguel.angello@sccuradars.local>
2023-10-09 14:00:02 -0300

    feat: request/return messages

--- src/Pages/Formularios/Historico/index.tsx
+++ src/Pages/Formularios/Historico/index.tsx
@@ -9,10 +9,17 @@ import { Search } from "lucide-react"; // Importação dos ícones
 import api from "../../../services/api";
 
 interface ResultOrderDataProps { // Essa interface é o tipo dos dados que a API retorna
+  order: number
+  requester: string
+  title: string
+}
+
+interface ResultHistoryDataProps { // Essa interface é o tipo dos dados que a API retorna
   date: string
   user: string
   history: string
 }
+
 
 export function Historico() {
   const [orderNumber, setOrderNumber] = useState<number>(0) // orderNumber é o estado que guarda o valor do input de número da ordem
@@ -21,7 +28,7 @@ export function Historico() {
    * resultOrderData é o estado que guarda os dados da ordem pesquisada, 
    * aqui ele é inicializado como um objeto vazio do tipo ResultOrderDataProps
    */
-  const [resultOrderData, setResultOrderData] = useState<ResultOrderDataProps[]>([])
+  const [resultHistoryData, setResultHistoryData] = useState<ResultHistoryDataProps[]>([])
 
   async function handleSearch() {
     console.log();
@@ -29,8 +36,8 @@ export function Historico() {
     await api // await é o método que espera a resposta da API
       .get(`/get/hist_ordem/${orderNumber}`) // .get é o método que faz a requisição para a API
       .then(response => {
-        console.log(response.data)
-        setResultOrderData(response.data) // setResultOrderData é o método que guarda os dados da ordem pesquisada no estado resultOrderData
+        console.log(response.data.history)
+        setResultHistoryData(response.data.history) // setResultHistoryData é o método que guarda os dados da ordem pesquisada no estado resultHistoryData
       }) // .then é o método que recebe a resposta da API e faz alguma coisa com ela
       .catch(error => {
         console.log(error)
@@ -52,12 +59,12 @@ export function Historico() {
       <ContainerChat>
         <h2>Histórico</h2>
         <ContainerMessages>
-          {resultOrderData.map(order => {
+          {resultHistoryData.map((history, index) => {
             return (
-              <Message>
-                <span>{order.user}</span>
-                <span>{order.history}</span>
-                <span>{order.date}</span>
+              <Message key={index}>
+                <span>{history.user}</span>
+                <span>{history.history}</span>
+                <span>{history.date}</span>
               </Message>
             )
           })}
