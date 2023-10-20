9a3a79bb7c4534aabcbbf92a06d4a9c3e4ced006
myguelangello <myguelangello624@gmail.com>
2023-10-20 03:13:54 -0300

    refactor: separate Button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,14 @@
+import { ButtonHTMLAttributes } from "react";
+import { ButtonStyled } from "./styles";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'search-icon' | 'search'
+}
+
+export function Button({ children, ...rest }: ButtonProps) {
+  return (
+    <ButtonStyled {...rest}>
+      {children}
+    </ButtonStyled>
+  )
+}
---
+++ src/components/Button/styles.ts
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+export const ButtonStyled = styled.button<{ variant?: string }>`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem 1rem;
+  font-size: 1rem;
+  line-height: 1;
+  font-weight: 500;
+  background-color: ${props => props.variant === 'search-icon' ? '#ce2929' : '#fff'};
+  color: #000;
+  cursor: pointer;
+  transition: all 0.25s ease;
+  position: ${props => props.variant === 'search-icon' ? 'absolute' : 'relative'};
+  right: ${props => props.variant === 'search-icon' ? '-4rem' : ''};
+  top: ${props => props.variant === 'search-icon' ? '0.5rem' : ''};
+	border: ${props => props.variant === 'search-icon' ? 'none' : '#71717a 1px solid'};
+  border-radius: ${props => props.variant === 'search-icon' ? '999px' : '0 0.5rem 0.5rem 0'};
+	
+  :hover {
+		background-color: ${props => props.variant === 'search-icon' ? '#ef4444' : '#e4e4e7'};
+		border: ${props => props.variant === 'search-icon' ? 'none' : '#71717a 1px solid'};
+	}
+
+  :focus {
+		box-shadow: 0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1);
+    border-radius: 0 0.5rem 0.5rem 0;
+	}
+
+  /* link */
+  ${props => props.variant === 'link' && `
+    margin-top: 0.5rem;
+    background-color: #e4e4e7;
+    color: #000;
+    border: transparent 1px solid;
+    padding: 0;
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+    text-decoration: underline;
+    cursor: pointer;
+    transition: all 0.25s ease;
+    :hover {
+      opacity: 0.9;
+    }
+  `}
+`;
