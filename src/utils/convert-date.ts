61b72d6133bebefe23e65f529e0996a3d536ac80
myguelangello <myguelangello624@gmail.com>
2023-10-20 00:02:23 -0300

    fix: adjust timzone of the date

--- src/utils/convert-date.ts
+++ src/utils/convert-date.ts
@@ -1,9 +1,10 @@
-import * as dayjs from 'dayjs';
+import dayjs from 'dayjs';
 import 'dayjs/locale/pt-br';
 
 dayjs.locale('pt-br');
 
 export function convertDate(date: string) {
-    return dayjs(date).locale('pt-br').format('ddd, DD [de] MMM. [de] YYYY [às] HH:mm');
+    // estou adicionando 3 horas para compensar o fuso horário GMT-3 (Brasília)
+    return dayjs(date).add(3, 'hour').locale('pt-br').format('ddd, DD [de] MMM. [de] YYYY [às] HH[h]mm');
 }
 
