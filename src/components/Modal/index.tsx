9b807a011a55ba81db9375e2a601f83d54912f77
Myguel Angello <myguel.angello@sccuradars.local>
2023-10-27 13:37:55 -0300

    feat: alter structure of Dialog Modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,45 +1,56 @@
-import { FocusEvent, MouseEvent, ReactNode } from 'react';
+import { FocusEvent, MouseEvent, ReactNode, forwardRef } from 'react';
 import { DialogContent, DialogDescription, DialogOverlay, DialogTitle, IconButton } from './styles';
-import * as Dialog from '@radix-ui/react-dialog';
+import * as DialogPrimitive from '@radix-ui/react-dialog';
 import { X } from 'lucide-react';
 
 export interface ModalProps {
   children: ReactNode;
-  open: boolean;
-  trigger: ReactNode;
   title: string;
   description?: string;
   overlay?: boolean;
-  setOpen: (open: boolean) => void;
   isInteractiveOutside?: boolean;
+  size: string | undefined
 }
 
-export function Modal({ open, setOpen, children, trigger, title,
-  description, overlay = true, isInteractiveOutside = true }: ModalProps) {
+interface Props extends DialogPrimitive.DialogProps {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+}
 
-  return (
-    <Dialog.Root open={open} onOpenChange={setOpen}>
+export const Content = forwardRef(
+  ({
+    children,
+    title,
+    size,
+    description,
+    overlay = true,
+    isInteractiveOutside = true,
+  }: ModalProps, forwardedRef) => (
+    <DialogPrimitive.Portal>
+      {overlay && <DialogOverlay />}
 
+      <DialogContent
+        style={{ width: size }}
+        ref={forwardedRef}
+        onInteractOutside={(event: any) => { !isInteractiveOutside && event.preventDefault() }}
+      >
+        <DialogTitle>{title}</DialogTitle>
+        <DialogDescription>{description}</DialogDescription>
 
-      <Dialog.Trigger asChild>{trigger}</Dialog.Trigger>
+        {children}
 
-      <Dialog.Portal>
-        {overlay && <DialogOverlay />}
+        <DialogPrimitive.Close asChild>
+          <IconButton aria-label='Close'>
+            <X size={20} />
+          </IconButton>
+        </DialogPrimitive.Close>
 
-        <DialogContent onInteractOutside={(event) => { !isInteractiveOutside && event.preventDefault() }}>
-          <DialogTitle>{title}</DialogTitle>
-          <DialogDescription>{description}</DialogDescription>
-
-          {children}
-
-          <Dialog.Close asChild>
-            <IconButton aria-label='Close'>
-              <X size={20} />
-            </IconButton>
-          </Dialog.Close>
-
-        </DialogContent>
-      </Dialog.Portal>
-    </Dialog.Root>
+      </DialogContent>
+    </DialogPrimitive.Portal>
   )
-}
+);
+
+export const Dialog = ({ open, setOpen, ...props }: Props) => (
+  <DialogPrimitive.Root open={open} onOpenChange={setOpen} {...props} />
+);
+export const Trigger = DialogPrimitive.Trigger;
