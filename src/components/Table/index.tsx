e2a35f90342bd13ae0a4e3e7b73f1c26a7ac6b9d
Myguel Angello <myguel.angello@sccuradars.local>
2024-01-16 16:02:38 -0300

    style: improve style and semantic Table

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,12 +1,12 @@
+import { AxiosError } from 'axios'
+import { EyeIcon } from 'lucide-react'
+import { toast } from 'react-toastify'
+import { ResultOrderDataProps } from '../../Pages/Formularios/Historico'
 import { useSearch } from '../../contexts/SearchContext'
 import { ApiDataProps } from '../../hooks/useFetch'
+import { configToastError } from '../../utils/toast-config'
 import { Button } from '../Button'
 import { Container, Th } from './styles'
-import { ResultOrderDataProps } from '../../Pages/Formularios/Historico'
-import { toast } from 'react-toastify'
-import { configToastError } from '../../utils/toast-config'
-import { EyeIcon } from 'lucide-react'
-import { AxiosError } from 'axios'
 
 import api from '../../services/api'
 
@@ -51,12 +51,12 @@ export default function Table({ data }: TableProps) {
     }
     return (
         <Container>
-            <table>
+            <table style={{ width: '100%' }}>
                 <thead>
                     <tr>
                         <Th>Número ordem</Th>
                         <Th>Título da ordem</Th>
-                        <Th>Ação</Th>
+                        <Th style={{ textAlign: 'center' }}>Ação</Th>
                     </tr>
                 </thead>
                 <tbody>
@@ -64,9 +64,11 @@ export default function Table({ data }: TableProps) {
                         <tr key={index}>
                             <td>{result.number}</td>
                             <td>{result.title}</td>
-                            <Button variant='reply' onClick={() => { handleSearch(result.number) }}>
-                                <EyeIcon size={20} />
-                            </Button>
+                            <td style={{ textAlign: 'center' }}>
+                                <Button variant='reply' onClick={() => handleSearch(result.number)}>
+                                    <EyeIcon size={20} />
+                                </Button>
+                            </td>
                         </tr>
                     )
                     )}
