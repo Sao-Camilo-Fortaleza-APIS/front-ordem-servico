0101608f93b3cf11546cf959866c818ecf3308b5
Myguel Angello Maciel de Abreu <myguel.angello@sccuradars.local>
2024-02-28 13:58:19 -0300

    feat(pre): Add search orders

--- src/routes/ViewOrders.tsx
+++ src/routes/ViewOrders.tsx
@@ -1,8 +1,44 @@
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { LogOut } from "lucide-react";
-import { Container, Header } from "../Styles/ViewOrders.styles";
+import { FormEvent, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import { Button } from "../components/Button";
+import { Order } from "../components/Order";
+import api from "../services/api";
+import { Container, Header } from "../styles/ViewOrders.styles";
 
 export function ViewOrders() {
+  const [searchParams, setSearchParams] = useSearchParams()
+  const [typeUser, setTypeUser] = useState('')
+  const [numberOrder, setNumberOrder] = useState(0)
+
+  const { data: tagsResponse, isLoading, isFetching } = useQuery({
+    queryKey: ["get-orders", typeUser, numberOrder],
+    queryFn: async () => {
+      await api.get(`/get/order_user/${typeUser}/${numberOrder}`)
+        .then(response => {
+          return response.data
+        })
+
+
+    },
+    placeholderData: keepPreviousData,
+  })
+
+  function handleFilter(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    setSearchParams(params => {
+      params.set('type', typeUser)
+      params.set('number', numberOrder.toString())
+
+      return params
+    })
+  }
+  if (isLoading) {
+    return null
+  }
+
   return (
     <Container>
       <Header>
@@ -17,9 +53,13 @@ export function ViewOrders() {
 
           <span>0</span>
         </div>
-        <div className="filter">
-          <Button>Em atendimento</Button>
-          <Button>Aguardando atendimento</Button>
+        <form onSubmit={handleFilter} className="filter">
+          <Button>EM ATENDIMENTO</Button>
+          <Button>AGUARDANDO</Button>
+        </form>
+
+        <div className="list-orders">
+          <Order />
         </div>
       </div>
     </Container>
