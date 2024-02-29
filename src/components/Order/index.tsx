79a5ab4ca906c8225a9c3b5a7bd8afc8665cacdf
myguelangello <myguelangello624@gmail.com>
2024-02-29 03:35:41 -0300

    feat: Add usage of props

--- src/components/Order/index.tsx
+++ src/components/Order/index.tsx
@@ -2,145 +2,43 @@ import * as Accordion from '@radix-ui/react-accordion';
 import { Clock, Hourglass, MapPin } from 'lucide-react';
 import { AccordionContent, AccordionTrigger, Container } from './styles';
 
-export function Order() {
-    return (
-        <Accordion.Root type='single' collapsible>
-            <Container>
-                <Accordion.Item value='item-1'>
-                    <AccordionTrigger>
-                        <div>
-                            <span className='title'>Computador sem internet aqui na uti</span>
-                            <span className='infos'>
-                                <Clock size={16} color='#a1a1aa' />
-                                28/02/2024 às 14:00
-                            </span>
-                            <span className='infos'>
-                                <MapPin size={16} color='#a1a1aa' />
-                                Pronto Atendimento Adulto
-                            </span>
-                        </div>
-                        <div className='icon-card'>
-                            <Hourglass size={24} color='#0EA5E9' />
-                        </div>
-                    </AccordionTrigger>
-                    <AccordionContent>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Dolores eos rem, molestiae itaque minima accusantium fugit
-                        quas in necessitatibus explicabo sequi.
-                    </AccordionContent>
-                </Accordion.Item><Accordion.Item value='item-2'>
-                    <AccordionTrigger>
-                        <div>
-                            <span className='title'>Computador sem internet aqui na uti</span>
-                            <span className='infos'>
-                                <Clock size={16} color='#a1a1aa' />
-                                28/02/2024 às 14:00
-                            </span>
-                            <span className='infos'>
-                                <MapPin size={16} color='#a1a1aa' />
-                                Pronto Atendimento Adulto
-                            </span>
-                        </div>
-                        <div className='icon-card'>
-                            <Hourglass size={24} color='#0EA5E9' />
-                        </div>
-                    </AccordionTrigger>
-                    <AccordionContent>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Dolores eos rem, molestiae itaque minima accusantium fugit
-                        quas in necessitatibus explicabo sequi.
-                    </AccordionContent>
-                </Accordion.Item><Accordion.Item value='item-3'>
-                    <AccordionTrigger>
-                        <div>
-                            <span className='title'>Computador sem internet aqui na uti</span>
-                            <span className='infos'>
-                                <Clock size={16} color='#a1a1aa' />
-                                28/02/2024 às 14:00
-                            </span>
-                            <span className='infos'>
-                                <MapPin size={16} color='#a1a1aa' />
-                                Pronto Atendimento Adulto
-                            </span>
-                        </div>
-                        <div className='icon-card'>
-                            <Hourglass size={24} color='#0EA5E9' />
-                        </div>
-                    </AccordionTrigger>
-                    <AccordionContent>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Dolores eos rem, molestiae itaque minima accusantium fugit
-                        quas in necessitatibus explicabo sequi.
-                    </AccordionContent>
-                </Accordion.Item><Accordion.Item value='item-4'>
-                    <AccordionTrigger>
-                        <div>
-                            <span className='title'>Computador sem internet aqui na uti</span>
-                            <span className='infos'>
-                                <Clock size={16} color='#a1a1aa' />
-                                28/02/2024 às 14:00
-                            </span>
-                            <span className='infos'>
-                                <MapPin size={16} color='#a1a1aa' />
-                                Pronto Atendimento Adulto
-                            </span>
-                        </div>
-                        <div className='icon-card'>
-                            <Hourglass size={24} color='#0EA5E9' />
-                        </div>
-                    </AccordionTrigger>
-                    <AccordionContent>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Dolores eos rem, molestiae itaque minima accusantium fugit
-                        quas in necessitatibus explicabo sequi.
-                    </AccordionContent>
-                </Accordion.Item><Accordion.Item value='item-5'>
-                    <AccordionTrigger>
-                        <div>
-                            <span className='title'>Computador sem internet aqui na uti</span>
-                            <span className='infos'>
-                                <Clock size={16} color='#a1a1aa' />
-                                28/02/2024 às 14:00
-                            </span>
-                            <span className='infos'>
-                                <MapPin size={16} color='#a1a1aa' />
-                                Pronto Atendimento Adulto
-                            </span>
-                        </div>
-                        <div className='icon-card'>
-                            <Hourglass size={24} color='#0EA5E9' />
-                        </div>
-                    </AccordionTrigger>
-                    <AccordionContent>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Dolores eos rem, molestiae itaque minima accusantium fugit
-                        quas in necessitatibus explicabo sequi.
-                    </AccordionContent>
-                </Accordion.Item><Accordion.Item value='item-6'>
-                    <AccordionTrigger>
-                        <div>
-                            <span className='title'>Computador sem internet aqui na uti</span>
-                            <span className='infos'>
-                                <Clock size={16} color='#a1a1aa' />
-                                28/02/2024 às 14:00
-                            </span>
-                            <span className='infos'>
-                                <MapPin size={16} color='#a1a1aa' />
-                                Pronto Atendimento Adulto
-                            </span>
-                        </div>
-                        <div className='icon-card'>
-                            <Hourglass size={24} color='#0EA5E9' />
-                        </div>
-                    </AccordionTrigger>
-                    <AccordionContent>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Dolores eos rem, molestiae itaque minima accusantium fugit
-                        quas in necessitatibus explicabo sequi.
-                    </AccordionContent>
-                </Accordion.Item>
+export interface OrderProps {
+    damage: string
+    date: string
+    location: string
+    number: number
+    requester: string
+}
 
-            </Container>
-        </Accordion.Root>
+export function Order(props: OrderProps) {
+    return (
+        <Container>
+            <Accordion.Item value={props.number.toString()}>
+                <AccordionTrigger>
+                    <div>
+                        <span className='title'>{props.damage}</span>
+                        <span className='infos'>
+                            <Clock size={16} color='#a1a1aa' />
+                            {props.date}
+                        </span>
+                        <span className='infos'>
+                            <MapPin size={16} color='#a1a1aa' />
+                            {props.location}
+                        </span>
+                    </div>
+                    <div className='icon-card'>
+                        <Hourglass size={24} color='#0EA5E9' />
+                    </div>
+                </AccordionTrigger>
+                <AccordionContent>
+                    <p>
+                        {props.damage}
+                    </p>
+                    <span>
+                        {props.requester}
+                    </span>
+                </AccordionContent>
+            </Accordion.Item>
+        </Container>
     )
 }
