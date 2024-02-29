0c016b2aa36e77aa43803dd3b3fb761ba2dac909
myguelangello <myguelangello624@gmail.com>
2024-02-29 02:37:35 -0300

    refactor: Refactor ViewOrders component

--- src/routes/ViewOrders.tsx
+++ src/routes/ViewOrders.tsx
@@ -1,44 +1,9 @@
-import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { LogOut } from "lucide-react";
-import { FormEvent, useState } from "react";
-import { useSearchParams } from "react-router-dom";
 import { Button } from "../components/Button";
 import { Order } from "../components/Order";
-import api from "../services/api";
 import { Container, Header } from "../styles/ViewOrders.styles";
 
 export function ViewOrders() {
-  const [searchParams, setSearchParams] = useSearchParams()
-  const [typeUser, setTypeUser] = useState('')
-  const [numberOrder, setNumberOrder] = useState(0)
-
-  const { data: tagsResponse, isLoading, isFetching } = useQuery({
-    queryKey: ["get-orders", typeUser, numberOrder],
-    queryFn: async () => {
-      await api.get(`/get/order_user/${typeUser}/${numberOrder}`)
-        .then(response => {
-          return response.data
-        })
-
-
-    },
-    placeholderData: keepPreviousData,
-  })
-
-  function handleFilter(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault()
-
-    setSearchParams(params => {
-      params.set('type', typeUser)
-      params.set('number', numberOrder.toString())
-
-      return params
-    })
-  }
-  if (isLoading) {
-    return null
-  }
-
   return (
     <Container>
       <Header>
@@ -53,7 +18,7 @@ export function ViewOrders() {
 
           <span>0</span>
         </div>
-        <form onSubmit={handleFilter} className="filter">
+        <form className="filter">
           <Button>EM ATENDIMENTO</Button>
           <Button>AGUARDANDO</Button>
         </form>
