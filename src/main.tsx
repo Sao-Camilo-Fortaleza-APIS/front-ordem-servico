3ae17f12faffc37443315c9077adec18cf583534
myguelangello <myguelangello624@gmail.com>
2024-03-21 23:56:48 -0300

    feat: Add protected route for ViewOrders

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.min.css';
 import App from './App';
 import { AjusteOk } from './Pages/Formularios/FormAjuste/AjusteSucess';
@@ -11,8 +11,18 @@ import { SignIn } from './routes/SignIn';
 import { ViewOrders } from './routes/ViewOrders';
 import { GlobalStyles } from './styles/global';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { Login } from './routes/Login';
+import { useUser } from './hooks/useUser';
 
 const queryClient = new QueryClient()
+
+function ProtectedRoute({ children }: PropsWithChildren) {
+  const { user } = useUser();
+  if (!user) return <Navigate to="/login" replace />
+
+  return <>{children}</>
+}
 
 const router = createBrowserRouter([
   {
@@ -37,8 +47,15 @@ const router = createBrowserRouter([
         element: <SignIn />,
       },
       {
+        path: '/login',
+        element: <Login />,
+      },
+      {
         path: '/ordens',
-        element: <ViewOrders />,
+        element:
+          <ProtectedRoute>
+            <ViewOrders />,
+          </ProtectedRoute>
       },
     ],
   },
@@ -50,6 +67,8 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <GlobalStyles />
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
 )
