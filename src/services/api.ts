96db1e102ef6b110a8efe71bdf0703384a6782ce
myguelangello <myguelangello624@gmail.com>
2024-03-21 23:58:21 -0300

    feat: Add token-based authentication to API requests

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,13 @@
 import axios from 'axios';
 
+const token = sessionStorage.getItem('token');
+
 const api = axios.create({
 	baseURL: `${import.meta.env.VITE_BASE_URL_HML}`,
 });
+
+if (token) {
+	api.defaults.headers['Authorization'] = `Bearer ${token}`;
+}
 
 export default api;
