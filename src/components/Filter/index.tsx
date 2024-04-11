86f76949ec8eec02d4c945da44ca9621dbc7aa5b
myguelangello <myguelangello624@gmail.com>
2024-04-11 02:42:22 -0300

    Refactor Filter component to use colorType variable

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -4,13 +4,20 @@ import { Button } from "../Button";
 interface Props extends HTMLAttributes<HTMLButtonElement> {
     title: string;
     isActive?: boolean;
-    type: 'byExecutor' | 'byPending';
+    type: 'do-executor' | 'sem-executor';
 };
 
 export function Filter({ title, type, isActive = false, ...rest }: Props) {
+    const colorType = type === 'do-executor' ? '#f59e0b' : '#60a5fa';
     return (
         <Button
             {...rest}
+            style={{
+                borderColor: colorType,
+                borderWidth: isActive ? 2 : 0,
+                color: isActive ? colorType : '#71717A',
+            }}
+
         >
             {title}
         </Button>
