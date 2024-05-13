242d4fca9b1a17305160d72aa8fabe9a44b7f5f9
myguelangello <myguelangello624@gmail.com>
2024-05-13 03:50:53 -0300

    refactor: Update Order component styles to be more responsive to the screen

--- src/components/Order/styles.ts
+++ src/components/Order/styles.ts
@@ -54,67 +54,73 @@ export const DialogDescription = styled(Dialog.Description)`
 `;
 
 export const OrderDetails = styled.div<{ color: string }>`
-    overflow: hidden;
-    margin-top: 1rem;
-    &:first-child {
-        margin-top: 0;
-        border-top-left-radius: 4px;
-        border-top-right-radius: 4px;
-    }
-
-    &:last-child {
-        border-bottom-left-radius: 4px;
-        border-bottom-right-radius: 4px;
-    } 
-    background-color: #f4f4f5;
     display: flex;
-    width: 100%;
-    height: 5.5rem;
-    flex-direction: row;
     align-items: center;
     justify-content: space-between;
     padding: 0.5rem 1rem;
-    margin-top: 0.5rem;
+    
+    background-color: #f4f4f5;
     border: none;
     border-radius: 0.313rem;
     border-left: 0.5rem solid;
     border-left-color: ${props => props.color};
-    overflow: hidden;
+
     cursor: pointer;
 
-    div {
+    div:first-child {
         display: flex;
         flex-direction: column;
         align-items: start;
         justify-content: center;
         gap: 0.1rem;
+        overflow: hidden;
 
         .title {
             font-size: 1rem;
             font-weight: 600;
             color: #71717a;
             text-align: left;
+            white-space: normal;
+            overflow: hidden;
         }
 
         .infos {
-            font-size: 0.875rem;
+             font-size: 0.875rem;
             display: flex;
             align-items: center;
+            justify-content: start;
             gap: 0.125rem;
             color: #a1a1aa;
-            text-align: left;
+            span{
+                text-align: left;
+                white-space: nowrap;
+                overflow: hidden;
+                text-overflow: ellipsis;
+            }
         }
     }
+    div.icon {
+        margin-left: 0.5rem;
+    }
+    // media queries smarthphone medium
+    @media (max-width: 426px) {
+        width: 100%;
+        padding: 0.5rem 0.5rem;
+        div {
+            .title {
+                font-size: 0.875rem;
+                font-weight: 400;
+            }
+            .infos span{
+                overflow: hidden;
+                white-space: nowrap;
+                text-overflow: ellipsis;
+            }
+        }
+        div.icon {
+            margin-left: 0.5rem;
+            margin-right: 0;
 
-    .icon {
-        background-color: #fafafa;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        padding: 0.5rem;
-        border-radius: 99990px; 
-        > {
-            background-color: ${props => props.color};
         }
     }
 `
