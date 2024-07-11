37505a4cdbdc08f03980995914efbed4ba76c184
Sao-Camilo-Fortaleza-APIS <sih@saocamilofortaleza.org.br>
2024-07-11 11:31:46 -0300

    chore: Rename VITE_BASE_URL_HML to VITE_BASE_URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const api = axios.create({
-	baseURL: `${import.meta.env.VITE_BASE_URL_HML}`,
+	baseURL: `${import.meta.env.VITE_BASE_URL}`,
 });
 api.interceptors.request.use(config => {
 	const token = Cookies.get('exec.token')
