a989ef13f5cb6a1a395c0443b7764f3fc8b3aab6
Myguel Angello <myguelangello624@gmail.com>
2024-08-09 10:28:37 -0300

    style: Update form margins and padding for consistency

--- src/components/TakeOrderForm/index.tsx
+++ src/components/TakeOrderForm/index.tsx
@@ -1,119 +1,123 @@
 import Cookies from "js-cookie";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import api from "../../services/api";
-import { FormStyled } from "./styles";
-import { useState } from "react";
 import { GroupProps, GroupResponse } from "../../routes/ViewOrders";
-
+import api from "../../services/api";
+import { Button } from "../Button";
+import { FormStyled } from "./styles";
 
 export function TakeOrderForm({ numberOrder }: { numberOrder: number }) {
-  const navigate = useNavigate()
-  const [open, setOpen] = useState(false)
-  const [allWorkgroups, setAllWorkgroups] = useState<GroupResponse>()
-  const [selectedGroup, setSelectedGroup] = useState()
+	const navigate = useNavigate()
+	const [open, setOpen] = useState(false)
+	const [allWorkgroups, setAllWorkgroups] = useState<GroupResponse>()
+	const [selectedGroup, setSelectedGroup] = useState()
 
-  let user = Cookies.get('user') ?? ''
+	let user = Cookies.get('user') ?? ''
 
-  async function fetchWorkgroups() {
-    await api.get(`/get/workgroup`)
-      .then((response) => {
-        console.log(response.data)
-        setAllWorkgroups(response.data)
-      })
+	async function fetchWorkgroups() {
+		await api.get(`/get/workgroup`)
+			.then((response) => {
+				console.log(response.data)
+				setAllWorkgroups(response.data)
+			})
 
-  }
+	}
 
-  async function handleSendOrderReply(event: any) {
-    event.preventDefault()
+	async function handleSendOrderReply(event: any) {
+		event.preventDefault()
 
-    try {
-      if (!user || user === '') {
-        Cookies.remove('user')
-        Cookies.remove('exec.token')
-        navigate('/signin')
-      } else if (!numberOrder) {
-        toast.error('Número da Ordem de Serviço não encontrado')
-        return
-      }
-      await api.post('/post/takeon', {
-        nr_order: numberOrder,
-        nm_user: user
-      })
-      window.location.reload()
+		try {
+			if (!user || user === '') {
+				Cookies.remove('user')
+				Cookies.remove('exec.token')
+				navigate('/signin')
+			} else if (!numberOrder) {
+				toast.error('Número da Ordem de Serviço não encontrado')
+				return
+			}
+			await api.post('/post/takeon', {
+				nr_order: numberOrder,
+				nm_user: user
+			})
+			window.location.reload()
 
-      toast.success('Ordem de Serviço assumida')
-    } catch (error) {
-      toast.error('Erro ao assumir Ordem de Serviço')
-      console.error(error)
-    }
-  }
+			toast.success('Ordem de Serviço assumida')
+		} catch (error) {
+			toast.error('Erro ao assumir Ordem de Serviço')
+			console.error(error)
+		}
+	}
 
-  async function handleTransferOrder() {
-    await api.post('post/transfer/workgroup', {
-      code_workgroup: selectedGroup,
-      nr_order: numberOrder,
-    }).then(() => {
-      toast.success("Ordem de Serviço transferida!")
-    }).catch((error) => {
-      console.log(error);
+	async function handleTransferOrder() {
+		await api.post('post/transfer/workgroup', {
+			code_workgroup: selectedGroup,
+			nr_order: numberOrder,
+		}).then(() => {
+			toast.success("Ordem de Serviço transferida!")
+		}).catch((error) => {
+			console.log(error);
 
-      toast.error("Ocorreu um erro")
-    })
-  }
+			toast.error("Ocorreu um erro")
+		})
+	}
 
-  const handleChange = (event: any) => {
-    setSelectedGroup(event.target.value);
-  };
+	const handleChange = (event: any) => {
+		setSelectedGroup(event.target.value);
+	};
 
-  return (
-    <FormStyled >
+	return (
+		<FormStyled >
 
-      <div id="takeon-transfer">
-        <button
-          type="submit"
-          onClick={handleSendOrderReply}>
-          Assumir
-        </button>
-        <button
-          type="button"
-          onClick={() => {
-            setOpen(true)
-            fetchWorkgroups()
-          }}>
-          Transferir
-        </button>
-      </div>
+			{!open && (
+				<div id="takeon-transfer">
+					<Button
+						type="submit"
+						onClick={handleSendOrderReply}>
+						Assumir
+					</Button>
+					<Button
+						type="button"
+						onClick={() => {
+							setOpen(true)
+							fetchWorkgroups()
+						}}>
+						Transferir
+					</Button>
+				</div>
+			)}
 
 
-      <div className="confirm-transfer">
-        {open && (
-          <>
-            <div id="select-group">
-              <label htmlFor="groups" style={{ width: '100%' }}>Selecione o grupo para transferir</label>
-              <select
-                className="select-group"
-                name="groups"
-                id="groups"
-                onChange={handleChange}
-                value={selectedGroup}
-              >
-                <option value="">Selecione...</option>
+			{open && (
+				<div className="confirm-transfer">
+					<div>
+						<label
+							htmlFor="groups"
+							style={{ width: '100%' }}
+						>
+							Selecione o grupo para transferir
 
-                {allWorkgroups && allWorkgroups.map((group: GroupProps) => {
-                  return <option key={group.code} value={group.code}>{group.describe}</option>
-                })}
+						</label>
+						<select
+							className="select-group"
+							name="groups"
+							id="groups"
+							onChange={handleChange}
+							value={selectedGroup}
+						>
+							<option value="">Selecione...</option>
+							{allWorkgroups && allWorkgroups.map((group: GroupProps) => {
+								return <option key={group.code} value={group.code}>{group.describe}</option>
+							})}
+						</select>
+					</div>
 
-              </select>
-            </div>
-
-            <div id="confirm-close">
-              <button onClick={handleTransferOrder}>Confirmar</button>
-              <button type="button" onClick={() => setOpen(false)}>Fechar</button>
-            </div>
-          </>
-        )}
-      </div>
-    </FormStyled>
-  )
+					<div id="confirm-close">
+						<button onClick={handleTransferOrder}>Confirmar</button>
+						<button type="button" onClick={() => setOpen(false)}>Fechar</button>
+					</div>
+				</div>
+			)}
+		</FormStyled>
+	)
 }
--- src/components/TakeOrderForm/styles.ts
+++ src/components/TakeOrderForm/styles.ts
@@ -1,44 +1,65 @@
 import styled from "styled-components";
 
 export const FormStyled = styled.form`
-  width: 100%;
-  display: flex; 
-  flex-direction: column;
-  align-items: center;
-  gap: 0.5rem;
+	width: 100%;
+	display: flex; 
+	flex-direction: column;
+	align-items: center;
+	gap: 0.5rem;
 
-  button {
-      padding: 0.5rem 1rem;
-      width: 100%;
-      color: #71717A;
-      font-weight: 600;
-      background: #d4d4d8;
-      border: 1px solid #d4d4d8;
-      border-radius: 4px;
-    }
-   select {
-    padding: 0.5rem 1rem;
-   } 
-   label{
-    color: #71717A;
-    font-weight: 600;
+	button {
+		padding: 0.5rem 1rem;
+		width: 100%;
+		color: #71717A;
+		font-weight: 600;
+		background: #d4d4d8;
+		border: 1px solid #d4d4d8;
+		border-radius: 4px;
+		transition: 0.3s;
 
-   }
+		:hover {
+			background: #71717A;
+			color: #fff;
+		}
+	}
+	select {
+		padding: 0.5rem 1rem;
+		width: 100%;
+	} 
+	label{
+		color: #71717A;
+		font-weight: 600;
+
+	}
    
-  #takeon-transfer {
-    display: flex;
-    flex-direction: row;
-    width: 100%;
-    gap: 0.5rem;
-  }
+	#takeon-transfer {
+		display: flex;
+		flex-direction: row;
+		width: 100%;
+		gap: 0.5rem;
+		padding: 1rem 2rem;
+	}
 
-  #select-group {
-    width: 100%;
-  }
+	.confirm-transfer {
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		gap: 0.5rem;
+		padding: 1rem 2rem;
+	}
 
-  #confirm-close {
-    margin-top: 0.5rem;
-    display: flex;
-    gap: 0.5rem;
-  }
+	 .select-group {
+		width: 100%;
+		font-weight: 500;
+		font-size: 1rem;
+		color: #71717A;
+		border: 1px solid #d4d4d8;
+		border-radius: 4px;
+	}
+
+	#confirm-close {
+		margin-top: 0.5rem;
+		display: flex;
+		gap: 0.5rem;
+	}
 `;
