e7c92c5cfcab07d15c0fb051c845726d84da5230
Docker <docker@app.sccuradars.local>
2025-02-25 11:06:33 +0000

    fix: Update imports from styles

---
+++ src/styles/ServiceOrderDetails.styles.ts
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    gap: 0.5rem;
+    margin: 0 0.5rem;
+    width: 100%;
+`;
+
+export const OrderDetails = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    justify-content: start;
+    font-size: 0.875rem;
+    color: #9ca3af;
+    gap: 0.5rem;
+    background-color: #fff;
+    padding: 1rem;
+    margin-top: 1rem;
+    border-radius: 1rem;
+    h4 {
+        font-size: 1.25rem;
+        font-weight: 600;
+        color: #71717a;
+        text-align: left;
+        white-space: normal;
+        overflow: hidden;
+    }
+
+    div {
+        display: flex;
+        flex-direction: column;
+        align-items: start;
+        justify-content: center;
+        gap: 0.1rem;
+        overflow: hidden;
+        font-size: 1rem;
+
+         .infos {
+             font-size: 1rem;
+            display: flex;
+            align-items: center;
+            justify-content: start;
+            gap: 0.125rem;
+            color: #a1a1aa;
+            span{
+                text-align: left;
+                white-space: nowrap;
+                overflow: hidden;
+                text-overflow: ellipsis;
+            }
+        }
+    }
+`
---
+++ src/styles/ViewOrders.styles.ts
@@ -0,0 +1,150 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 100vw;
+  overflow: hidden;
+  overflow-Y: scroll;
+  padding: 0 0 5rem 0;
+ 
+
+  .wrapper {
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.5rem 1.5rem;
+
+    .list-orders {
+      width: 100%;
+      margin-bottom: 5rem;
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+    }
+  }
+
+  .legend {
+      display: flex;
+      gap: 0.5rem;
+      align-items: center;
+    }
+
+  @media (max-width: 426px) {
+    .wrapper {
+      padding: 0.5rem 0.75rem;
+    }
+  }
+
+  .select-group {
+    width: 100%;
+    height: 2.5rem;
+    padding: 0.5rem;
+    font-size: 1rem;
+    font-weight: 500;
+    color: #71717a;
+    border: 1px solid #d4d4d8;
+    border-radius: 4px;
+    background: #f4f4f5;
+  }
+
+  .quantidade, .label-groups {
+    width: 100%;
+    display: flex;
+    align-items: center; 
+    justify-content: start;
+    gap: 0.25rem;
+    margin-top: 1rem;
+    font-size: 1.125rem;
+    font-weight: 500;
+    color: #a1a1aa;
+    // color: #71717a;
+
+    span{
+      display: flex;
+      align-items: center;
+    }
+
+    .animate-spin {
+        animation: spin 1s linear infinite;
+
+        @keyframes spin {
+        from {
+            transform: rotate(0deg);
+        }
+        to {
+            transform: rotate(360deg);
+        }
+        }
+    }
+  }
+
+  .filter {
+    width: 100%;
+    display: flex;
+    align-items: center; 
+    justify-content: space-between;
+    gap: 0.25rem;
+    margin-top: 1rem;
+
+    button {
+      width: 100%;
+      font-size: 0.75rem;
+      font-weight: 400;
+      font-family: inherit;
+      border-radius: 4px;
+      background: #d4d4d8;
+      border: 2px solid #d4d4d8;
+    }
+  }
+`;
+
+export const Header = styled.header`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  padding-right: 1rem;
+  background-color: #fff;
+  border-bottom: 1px solid #f4f4f5;
+  img {
+    height: 4rem;
+    width: 6rem;
+  }
+
+  .hero {
+    display: flex;
+    align-items: center;
+    gap: 0.875rem;
+    font-size: 1rem;
+    color: #71717a;
+
+    .user-name {
+      display: flex;
+      font-weight: 400;
+      font-size: 0.875rem;
+      border-right: 1px solid #a1a1aa;
+      padding: 0.5rem 0.875rem 0.5rem 0;
+    }
+    
+    .logout {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 0.25rem;
+      color: #a1a1aa;
+      font-size: 0.875rem;
+      font-weight: 400;
+      cursor: pointer;
+      background-color: transparent;
+      border: none;
+
+      :hover {
+        color: #71717a;
+      }
+    }
+  }
+`;
