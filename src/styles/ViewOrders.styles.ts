0b98799bb33c851671ef86d9a563bb534cf4bc10
Myguel Angello <sih@saocamilofortaleza.org.br>
2025-04-11 08:51:27 -0300

    feat: Add search by CPF and remove consoles unnecessary

--- src/styles/ViewOrders.styles.ts
+++ src/styles/ViewOrders.styles.ts
@@ -67,19 +67,6 @@ export const Container = styled.div`
       display: flex;
       align-items: center;
     }
-
-    .animate-spin {
-        animation: spin 1s linear infinite;
-
-        @keyframes spin {
-        from {
-            transform: rotate(0deg);
-        }
-        to {
-            transform: rotate(360deg);
-        }
-        }
-    }
   }
 
   .filter {
